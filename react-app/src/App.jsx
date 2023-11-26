87590881757c3b0ac5f743e1fc6adc3c30166a82
ekwunifeifenna <ifennaekwunife73@gmail.com>
2023-11-25 21:09:21 -0500

    Complete first iteration of web design add search by filters(upcoming)

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,15 +1,68 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
 
-import './App.css'
+import "./App.css"
+import SearchIcon from "./search.svg"
+import MovieCard from "./components/MovieCard";
 
-function App() {
- 
+const API_URL = "http://www.omdbapi.com?apikey=9348f70";
+const App = () => { 
 
-  return (
-    <>
-      
-    </>
-  )
-}
+    const [movies , setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState([])
 
-export default App
+    const searchMovies = async (title) =>{
+        const response = await fetch(`${API_URL}&s=${title}`)
+
+        const data = await response.json()
+        setMovies(data.Search)
+
+    }
+
+    useEffect(()=>{
+        searchMovies("Superman")
+
+    },[])
+
+  return <div className="App">
+    <h1>MovieWorld</h1>
+    <div className="search">
+        <input 
+        placeholder="search for movies"
+        value={searchTerm}
+        onChange={(e)=>setSearchTerm(e.target.value)}
+        />
+        <img 
+        src={SearchIcon}
+        alt="search movies"
+        onClick={()=>searchMovies(searchTerm)}/>
+    </div>
+
+    {
+        movies?.length >0 
+        ? (<div className="container">
+            {
+                movies.map((movie)=>(
+                    <MovieCard 
+                    key={movie.imdbID}
+                    Year = {movie.Year}
+                    Poster = {movie.Poster}
+                    Title = {movie.Title}
+                    Type = {movie.Type}
+                    
+                    />
+                ))
+            }
+        
+            </div>) 
+            : (
+                <div className="empty">
+                    <h2>No movies found</h2>
+                </div>
+            )
+    }
+
+    
+  </div>;
+};
+
+export default App;
